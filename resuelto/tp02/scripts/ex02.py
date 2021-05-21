70edf2ff18643154d13f07fc6a2d7b2cce888412
MartinAramayo <gabriel.m.aramayo@gmail.com>
2021-05-20 23:50:28 -0300

    ✨ Add P1, Add P2, Add p5

---
+++ resuelto/tp02/scripts/ex02.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python ex02a.py")
--- resuelto/tp02/scripts/ex02b.py
+++ resuelto/tp02/scripts/ex02b.py
@@ -35,13 +35,26 @@ def axes_no_corner(ax):
     ax.get_yaxis().tick_left()
     
 ## Start
-def logistical(y, t, r, K):
-    dydt = r * y * (1 - y / K) 
-    return dydt
-
-def sterile_logistical(N, t, a, b, n, K):
+def birth_death_all_logistic(N, t, a, b, n, K):
     dNdt = N * ( a * N / (N+n) - b - K * (N + n) )
     return dNdt
+
+def birth_death_sterile_logistic(N, t, a, b, n, K):
+    dndt = -b * n - K * (n + N) * n
+    return dndt
+
+def custom_logistic(N, t, a, g, n, K):
+    dndt = -g * N - K * (n + N) * n
+    return dndt
+
+def sterile_logistical(y, t, a, b, k):
+    n, N = y
+    aux_kargs = {'N':N, 't':t, 'a':a, 'b':b, 'n':n, 'k':k}
+    dydt = [
+        birth_death_sterile_logistic(**aux_kargs),
+        birth_death_all_logistic(**aux_kargs)
+        ]
+    return dydt
 
 # interactive plots
 # plt.ion()
@@ -51,7 +64,7 @@ def sterile_logistical(N, t, a, b, n, K):
 a = 0.4
 b = 0.1
 K = 1
-t = np.linspace(0, 500, num=500)
+t = np.linspace(0, 600, num=600)
 aux_kargs = {
     'func': sterile_logistical,
     'y0': (N0:=K - 0.5),
@@ -66,10 +79,10 @@ ax.plot(t,sol_all)
 pseudo_capacidad = sol_all[-1]
 
 ax.axhline(pseudo_capacidad/1)
-ax.text(100, pseudo_capacidad/1 + 0.01, f'y={pseudo_capacidad/1:.3E}')
+ax.text(100, pseudo_capacidad/1 + 0.01, f'y={pseudo_capacidad/1:.1E}')
 
 ax.axhline(pseudo_capacidad/4)
-ax.text(-25, pseudo_capacidad/4 - 0.02, f'y={pseudo_capacidad/4:.3E}')
+ax.text(-25, pseudo_capacidad/4 - 0.02, f'y={pseudo_capacidad/4:.1E}')
 
 ## the array of parameters to numerically run
 # array_n = np.linspace(0, N0/9, num=20)
@@ -90,7 +103,6 @@ for j, n in np.ndenumerate(array_n):
 
     sol_all = odeint(**aux_kargs).flatten()
 
-    # my_label = f'$n$ = {n:3.2f}'
     my_label = f'$n$ = {n:.2E}'
     ax.plot(t, sol_all, label=my_label, color=colors[j[0]])
 
@@ -101,14 +113,12 @@ lgd = ax.legend(ncol=1,
                 loc='center left')
 ax.set_ylabel("Población $N$")
 ax.set_xlabel("tiempo")
-# ax.set_title(f'$a$ = {a:3.2f}, $b$ = {b:4.3f}, $n$ = {n:4.3f}, $K$ = {K:4.3f}')
 ax.set_title(f'$a$ = {a:3.2f}, $b$ = {b:4.3f}, $K$ = {K:4.3f}')
-# ax.set_title(f'$a$ = {a:3.2f}, $b$ = {b:4.3f}, $n$ = {n:.3E}, $K$ = {K:4.3f}')
 axes_no_corner(ax)
 
-fig.savefig('../figuras/ex5' + '.pdf',
+fig.savefig('../figuras/ex02b' + '.pdf',
             format='pdf',
             bbox_extra_artists=(lgd,),
             bbox_inches='tight')
 
-# plt.close('all')
+plt.close('all')
